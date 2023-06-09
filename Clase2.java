f4e596c815604972e867984b146c2d91eb459f07
Exe <exequelbaez@gmail.com>
2023-06-09 09:09:25 -0300

    busqueda pais

---
+++ Clase2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Clase2 {
+    public static void main(String[] args){
+
+        Scanner entrada = new Scanner(System.in);
+
+        //Busqueda pais
+
+        System.out.println("Ingrese un pais");
+
+        String pais = entrada.next();
+
+        System.out.println("https://www.google.com/maps/search/"+pais);
+
+
+
+    }
+}
