9403adf420e60f13d4ee1059ee2142cfdc2b9592
exeq33 <135068420+exeq33@users.noreply.github.com>
2023-06-30 13:41:42 -0300

    eulerEj1

---
+++ EulerEj3Primos/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Long numero = 600851475143L;
+        Long resultado;
+
+        for(Long i=1L; i<=10000; i++){
+            if(numero % i == 0){
+                System.out.println(resultado = i);
+            }
+        }
+
+    }
+}
